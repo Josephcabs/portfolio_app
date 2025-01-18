148fa54893ba7b3cd90829e045ea0641cdc751bf
Joseph <jxcaballero244@gmail.com>
2025-01-18 09:56:56 -0500

    chore: adds comment to stepper to reduce confusion on why it is there

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -1,52 +1,54 @@
-'use client'
-import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import MobileStepper from '@mui/material/MobileStepper';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import { useState } from 'react';
+// TODO: Add Stepper functionality to the portfolio
 
-export default function Stepper() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = useState(0);
+// 'use client'
+// import * as React from 'react';
+// import { useTheme } from '@mui/material/styles';
+// import MobileStepper from '@mui/material/MobileStepper';
+// import Button from '@mui/material/Button';
+// import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+// import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+// import { useState } from 'react';
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+// export default function Stepper() {
+//   const theme = useTheme();
+//   const [activeStep, setActiveStep] = useState(0);
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+//   const handleNext = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+//   };
 
-  return (
-    <MobileStepper
-      variant="dots"
-      steps={6}
-      position="static"
-      activeStep={activeStep}
-      className='bg-purple-600'
-      sx={{ maxWidth: 400, flexGrow: 1 }}
-      nextButton={
-        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
-          Next
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowLeft />
-          ) : (
-            <KeyboardArrowRight />
-          )}
-        </Button>
-      }
-      backButton={
-        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-          {theme.direction === 'rtl' ? (
-            <KeyboardArrowRight />
-          ) : (
-            <KeyboardArrowLeft />
-          )}
-          Back
-        </Button>
-      }
-    />
-  );
-}
+//   const handleBack = () => {
+//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+//   };
+
+//   return (
+//     <MobileStepper
+//       variant="dots"
+//       steps={6}
+//       position="static"
+//       activeStep={activeStep}
+//       className='bg-purple-600'
+//       sx={{ maxWidth: 400, flexGrow: 1 }}
+//       nextButton={
+//         <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+//           Next
+//           {theme.direction === 'rtl' ? (
+//             <KeyboardArrowLeft />
+//           ) : (
+//             <KeyboardArrowRight />
+//           )}
+//         </Button>
+//       }
+//       backButton={
+//         <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+//           {theme.direction === 'rtl' ? (
+//             <KeyboardArrowRight />
+//           ) : (
+//             <KeyboardArrowLeft />
+//           )}
+//           Back
+//         </Button>
+//       }
+//     />
+//   );
+// }
