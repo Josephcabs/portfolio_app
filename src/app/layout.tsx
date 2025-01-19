4d999e9683c1df2e832ec6dc786edba81c1f28f9
Joseph <jxcaballero244@gmail.com>
2025-01-19 13:53:54 -0500

    makes the deploymeny process succeed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Links from "@/components/Links";
 import Header from "@/components/Header";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
