aec597a33100b811dce8c74563cc543aac607faa
Joseph <jxcaballero244@gmail.com>
2025-01-25 17:07:21 -0500

    feat: adds barebones prop for provider

--- src/lib/provider/Provider.tsx
+++ src/lib/provider/Provider.tsx
@@ -1,6 +1,7 @@
 interface childrenProps {
   children: React.ReactNode;
   color: string;
+  setColor?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 //TODO - finish the provider handling for color
